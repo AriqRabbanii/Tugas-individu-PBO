02c46625e27c96ee52af9fa625fe64c174efa25a
AriqRabbanii <ariiqrabbanii@gmail.com>
2025-12-10 15:08:45 +0700

    Add Penumpang class with attributes and constructor

--- Penumpang.java
+++ Penumpang.java
@@ -1,1 +1,17 @@
+public class Penumpang {
+    private int id;
+    private int umur;
+    private boolean hamil;
+    private int saldo;
+    private String nama; 
+    
+    private static final int ONGKOS_BUS = 2000;
 
+    public Penumpang(int id, String nama, int umur, boolean hamil) {
+        this.id = id;
+        this.nama = nama;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = 10000; 
+    }
+}
