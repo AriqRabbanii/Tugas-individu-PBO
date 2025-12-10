dd05fe75b90d39372b18644493f156b77188e4cb
AriqRabbanii <ariiqrabbanii@gmail.com>
2025-12-10 15:20:28 +0700

    Refactor passenger management in TestBus

--- TestBus.java
+++ TestBus.java
@@ -12,6 +12,38 @@ public class TestBus {
         transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Otong", 18, false)); 
         transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Atok Dalang", 68, false)); 
         transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Ucup", 19, false)); 
-        transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "
+        transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Memet", 25, true)); 
+        System.out.println("--------------------------------------\n");
+                int pilihan;
+        do {
+            tampilkanMenu();
+            if (scanner.hasNextInt()) {
+                pilihan = scanner.nextInt();
+                scanner.nextLine(); 
+                
+                switch (pilihan) {
+                    case 1:
+                        naikkanPenumpangBaru();
+                        break;
+                    case 2:
+                        turunkanPenumpang();
+                        break;
+                    case 3:
+                        lihatPenumpang();
+                        break;
+                    case 0:
+                        System.out.println("Terima kasih. Simulasi diakhiri.");
+                        break;
+                    default:
+                        System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+                }
+            } else {
+                System.out.println("Input harus berupa angka menu.");
+                scanner.nextLine();
+                pilihan = -1;
+            }
+        } while (pilihan != 0);
+        
+        scanner.close();
     }
 }
