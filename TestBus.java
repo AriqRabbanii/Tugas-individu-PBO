407b1af3c5302112ffe0737d9dc8926316ce2d94
AriqRabbanii <ariiqrabbanii@gmail.com>
2025-12-10 15:18:47 +0700

    Set up initial bus and passenger data
    Initialize bus and passengers for simulation.

--- TestBus.java
+++ TestBus.java
@@ -1,4 +1,17 @@
 import java.util.Scanner;
 
 public class TestBus {
+      
+    private static Bus transKoetaradja = new Bus();
+    private static Scanner scanner = new Scanner(System.in);
+    private static int idCounter = 1;
+
+    public static void main(String[] args) {
+        // Data Awal Simulasi
+        System.out.println("--- Inisialisasi Bus dan Penumpang ---");
+        transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Otong", 18, false)); 
+        transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Atok Dalang", 68, false)); 
+        transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "Ucup", 19, false)); 
+        transKoetaradja.naikkanPenumpang(new Penumpang(idCounter++, "
+    }
 }
